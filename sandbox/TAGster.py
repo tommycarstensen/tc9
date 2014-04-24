b6e1fdc7b41f30d56320ecd8e6dfae382691a502
Tommy Carstensen <tc9@sanger.ac.uk>
2014-04-24 17:50:38 +0100

    Adding a --max_coverage_regional option after discussions with Manj and Deepti. This argument will prevent selection of tag SNPs from a region, once the coverage in that region exceeds a threshold.

--- sandbox/TAGster.py
+++ sandbox/TAGster.py
@@ -2,14 +2,10 @@
 
 ## Tommy Carstensen, Wellcome Trust Sanger Institute, April 2014
 
-## the script currently does not allow multiple chromosomes to be present in a single haps file
-
-## the script favors selection of SNPs in regions with low recombination rates
-
 import argparse
-import collections
 import os
 import sys
+import collections
 import gzip
 
 
@@ -19,13 +15,13 @@ def main():
     d_args = argparser()
 
     ## Declare pseudo matrix file name.
-    file_matrix = d_args['out']+'.LDmatrix'
     ## Declare dictionary to be populated with
     ## IDs as keys and byte positions as values
     d_tell = {}
+    d_cnt1 = {}
+    d_cnt2 = {}
     ## Open pseudo matrix file.
-    with open(file_matrix,'w') as f_matrix:
-        cnt = collections.Counter()
+    with open(d_args['out']+'.LDmatrix', 'w') as f_matrix:
         ## Loop over input files.
         for file in d_args['in']:
             print(file)
@@ -33,7 +29,7 @@ def main():
             ## Declare temporary LD dictionary.
             d_LD = {}
             ## Open input file.
-            with gzip.open(file) as f:
+            with gzip.open(file,'rt') as f:
                 ## Loop over input file lines.
                 for line in f:
                     l = line.rstrip().split()
@@ -57,9 +53,14 @@ def main():
                     ## Parse SNP IDs.
                     ID1 = l[0]
                     ID2 = l[1]
-                    ## Append count across populations. 
-                    cnt[ID1] += 1
-                    cnt[ID2] += 1
+                    ## Append count across populations.
+                    for ID in (ID1,ID2):
+                        try:
+                            d_cnt1[ID] += 1
+                            d_cnt2[ID] += 1
+                        except KeyError:
+                            d_cnt1[ID] = 1
+                            d_cnt2[ID] = 1
                     ## Append SNPs in LD to temporary LD dictionary.
                     for IDa,IDb in ((ID1,ID2),(ID2,ID1)):
                         try:
@@ -90,7 +91,7 @@ def main():
         ## Close pseudo matrix file.
         pass
 
-    print('len(setS)', len(d_tell.keys()), len(cnt.items()))
+    print('len(setS)', len(d_tell.keys()), len(d_cnt1.keys()), len(d_cnt2.keys()))
     setT = set()
     setQ = set()
     if d_args['preselected']:
@@ -100,15 +101,17 @@ def main():
                 setT.add(ID)
                 setQ.append(allinLD)
                 setP.remove(ID)
-                del cnt[ID]
+                del d_cnt1[ID]
                 for ID2 in d_LD[ID]:
                     d_LD[ID2].remove(ID)
-                    cnt[ID2] -= 1
+                    d_cnt1[ID2] -= 1
                 del d_LD[ID]
 ##                setT = set(f.read().rstrip().split('\n'))
 
+    ## Declare dictionary to be populated with
+    ## counts as keys and sets of IDs as values
     d_cnt2IDs = {}
-    for ID,count in cnt.items():
+    for ID,count in d_cnt1.items():
         try:
             d_cnt2IDs[count].add(ID)
         except KeyError:
@@ -117,42 +120,7 @@ def main():
 
     d_args['max_tagSNP'] = min(d_args['max_tagSNP'],len(d_tell.keys()))
 
-    most_common_key = max(d_cnt2IDs.keys())
-    with open(file_matrix) as f:
-        while len(setT) < d_args['max_tagSNP']:
-            ## Find the most common element.
-            while True:
-                try:
-                    ID = d_cnt2IDs[most_common_key].pop()
-                    break
-                except KeyError:
-                    del d_cnt2IDs[most_common_key]
-                    try:
-                        most_common_key = max(d_cnt2IDs.keys())
-                    except ValueError:
-                        stopshouldnothappen
-                        break
-            setT |= set([ID])
-            del cnt[ID]
-            ## File seek is the second slowest step in this loop.
-            for seek in d_tell[ID]:
-                f.seek(seek)
-                line = f.readline()
-                for ID2 in line.rstrip().split():
-                    ## tag SNP
-                    if ID2 in setT:
-                        continue
-                    ## tagged
-                    count = cnt[ID2]
-                    d_cnt2IDs[count].remove(ID2)
-                    try:
-                        d_cnt2IDs[count-1].add(ID2)
-                    except KeyError:
-                        d_cnt2IDs[count-1] = set([ID2])
-                    cnt[ID2] -= 1
-                    setQ.add(ID2) ## tmp!!!
-            del d_tell[ID]
-            sys.stdout.flush()
+    select_tag_SNPs(d_args, d_cnt2IDs, setT, setQ, d_cnt1, d_cnt2, d_tell)
 
     with open(d_args['out']+'.tagSNPs','w') as f:
         for ID in setT:
@@ -162,14 +130,74 @@ def main():
             f.write('%s\n' %(ID))
 
     ## Clean up large temporary files.
-    os.remove(file_matrix)
+    os.remove(d_args['out']+'.LDmatrix')
 
-    print('cnt[ID]',cnt[ID])
     print('len(setT)',len(setT))
     print('len(setQ)',len(setQ))
-    print('len(setS)', len(d_tell.keys()), len(cnt.items()))
+    print(
+        'len(setS)', len(d_tell.keys()),
+        len(d_cnt1.keys()), len(d_cnt2.keys()))
 
     return
+
+
+def select_tag_SNPs(d_args, d_cnt2IDs, setT, setQ, d_cnt1, d_cnt2, d_tell):
+    
+    most_common_key = max(d_cnt2IDs.keys())
+    with open(d_args['out']+'.LDmatrix') as f:
+        while len(setT) < d_args['max_tagSNP']:
+            ## Find the most common element.
+            ## collections.Counter.most_common(1) is too slow.
+            while True:
+                try:
+                    ID = d_cnt2IDs[most_common_key].pop()
+                    break
+                except KeyError:
+                    del d_cnt2IDs[most_common_key]
+                    try:
+                        most_common_key = max(d_cnt2IDs.keys())
+                    except ValueError:
+                        ## Dictionary exhausted.
+                        if not d_cnt2IDs:
+                            return setT, setQ
+                        stopshouldnothappen
+                        break
+            if (
+                (d_cnt2[ID]-d_cnt1[ID])/d_cnt2[ID]
+                > d_args['max_coverage_regional']):
+                del d_cnt1[ID]
+                del d_tell[ID]
+                continue
+            else:
+                setT |= set([ID])
+                del d_cnt1[ID]
+                pass
+            ## File seek is the second slowest step in this loop.
+            for seek in d_tell[ID]:
+                f.seek(seek)
+                line = f.readline()
+                for ID2 in line.rstrip().split():
+                    ## tag SNP
+                    if ID2 in setT:
+                        continue
+                    ## tagged
+                    try:
+                        count = d_cnt1[ID2]
+                    ## max_coverage_regional
+                    except KeyError:
+                        continue
+                    d_cnt2IDs[count].remove(ID2)
+                    try:
+                        d_cnt2IDs[count-1].add(ID2)
+                    except KeyError:
+                        d_cnt2IDs[count-1] = set([ID2])
+                    d_cnt1[ID2] -= 1
+                    if d_cnt1[ID2] < 0: print(ID,ID2,d_cnt1[ID2]); stop ## tmp!!!
+                    setQ.add(ID2) ## tmp!!!
+            del d_tell[ID]
+            sys.stdout.flush()
+
+    return setT, setQ
 
 
 def argparser():
@@ -183,6 +211,7 @@ def argparser():
     parser.add_argument('--window', type=int, default=250000)
     parser.add_argument('--preselected')
     parser.add_argument('--max_tagSNP', type=int, default=1000000)
+    parser.add_argument('--max_coverage_regional', type=float, default=.2)
 
     d_args = vars(parser.parse_args())
 
