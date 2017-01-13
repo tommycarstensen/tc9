e3f7d6ba5848dab024d3a9b6501092dace286864
Tommy Carstensen <tommy.carstensen@gmail.com>
2017-01-13 20:31:53 +0000

    Took into account that 24 samples from Hologic batch 12 and Coriell batches 4-6 were moved from plate 6 to plate 7.

--- projects/adrp1/samples_for_lanes.py
+++ projects/adrp1/samples_for_lanes.py
@@ -25,7 +25,6 @@ n_samples_per_lane = 12
 print('#n_Hologic_batches', n_Hologic_batches)
 print('#n_Coriell_batches', n_Coriell_batches)
 print('#n_Sanger_plates', n_Sanger_plates)
-print()
 
 d = {hb: {cb: [] for cb in range(1, n_cb+1)} for hb in range(1, n_hb+1)}
 d_ID2pop = {}
@@ -33,6 +32,7 @@ d_plate = {
     plate: {
         'init': Counter(), 'curr': Counter()} for plate in range(
             1, n_plates+1)}
+d_ID2well = {}
 
 for row_idx in range(1, xl_sheet.nrows):
     ID = Coriell_ID = xl_sheet.cell(row_idx, 0).value
@@ -40,24 +40,46 @@ for row_idx in range(1, xl_sheet.nrows):
     cb = Coriell_Batch = xl_sheet.cell(row_idx, 2).value
     hb = Hologic_Batch = xl_sheet.cell(row_idx, 3).value
     well = xl_sheet.cell(row_idx, 5).value
-    d[hb][cb].append(ID)
-    d[hb][cb].append(ID)
     d_ID2pop[ID] = pop
+    d_ID2well[ID] = well
     plate = ((hb-1)//2)+1
+    ## Half of Hologic batch 12 went to plate 7 (i.e. wells A10-H12).
     if plate == 6 and int(well[1:]) >= 10:
         plate = 7
-##    d_plate[plate]['init'][pop] += 2
-##    d_plate[plate]['curr'][pop] += 2
-    d_plate[plate]['init'][pop] += 1
-    d_plate[plate]['curr'][pop] += 1
+    ## Each sample occurs in one lane only according to Cristina/2017Jan13.
+    n_lanes_per_sample = 1
+    for x in range(n_lanes_per_sample):
+        d_plate[plate]['init'][pop] += 1
+        d_plate[plate]['curr'][pop] += 1
+        d[hb][cb].append(ID)
 
+i_lane = 1
+##range_cb_6_11 = random.shuffle(list(range(n_cb))*int((n_wells/2)/n_cb))
 for plate in range(1, n_plates+1):
-    n_samples = int(sum(d_plate[plate]['init'].values()) / 2)
-    n_lanes = int(n_samples / n_average_samples_per_lane)
+    n_samples = sum(d_plate[plate]['init'].values())
+    n_lanes = int(n_samples / n_samples_per_lane)
     for lane in range(1, n_lanes+1):
         selected_pops = []
-        for hb in range(plate*2-1, min(plate*2, n_hb)+1):
-            for cb in range(1, n_cb+1):
+        if plate == 7:
+            range_hb = range(12, 13+1)
+        else:
+            range_hb = range(plate*2-1, min(plate*2, n_hb)+1)
+        for hb in range_hb:
+            if plate == 6 and hb == 12:
+                ## Half of Hologic batch 12 went to plate 7 (i.e. wells A10-H12).
+                range_cb = list(list(range(1, int(n_cb/2)+1))*int((n_wells/2)/n_cb))[(lane-1)*int(((n_wells/2)/n_cb)/2):lane*int(((n_wells/2)/n_cb)/2)]
+            elif plate == 6 and hb == 11:
+##                ## Random sampling.
+##                range_cb = range_cb_6_11[(lane-1)*6:lane*6]
+                ## Systematic sampling.
+                range_cb = list(list(range(1, n_cb+1))*int((n_wells/2)/n_cb))[(lane-1)*int((n_wells/2)/n_cb):lane*int((n_wells/2)/n_cb)]
+            elif plate == 7 and hb == 12:
+                ## Half of Hologic batch 12 went to plate 7 (i.e. wells A10-H12).
+##                range_cb = list(list(range(int(n_cb/2)+1, n_cb+1))*int((n_wells/2)/n_cb))[(lane-1)*int(((n_wells/2)/n_cb)/2):lane*int(((n_wells/2)/n_cb)/2)]
+                range_cb = list(range(int(n_cb/2)+1, n_cb+1))*2
+            else:
+                range_cb = range(1, n_cb+1)
+            for cb in range_cb:
                 ## Count the populations in the remaining set of samples.
                 c = Counter([d_ID2pop[ID] for ID in d[hb][cb]])
                 ## (Re)set counter.
@@ -84,8 +106,21 @@ for plate in range(1, n_plates+1):
                 d_plate[plate]['curr'][pop] -= 1
                 d[hb][cb].remove(ID)
                 ## Print lane for each ID.
-                print(ID, d_ID2pop[ID], cb, hb, lane, sep='\t')
+                print(
+                    ID, d_ID2pop[ID], 'CB'+str(cb), 'HB'+str(hb),
+                    'lane'+str(lane), 'lane'+str(i_lane),
+                    'plate'+str(plate), d_ID2well[ID], sep='\t',
+                    )
                 selected_pops.append(d_ID2pop[ID])
+                ## Continue loop over cb_range.
+                continue
+            ## Continue loop over hb_range.
+            continue
+        ## Continue loop over lanes.
+        i_lane += 1
+        continue
+    ## Continue loop over plates.
+    continue
 ##        ## Print a summary for the lane.
 ##        print(
 ##            lane+(plate-1)*int(n_wells/n_average_samples_per_lane),
