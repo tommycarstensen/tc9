377d707fe870a013e920971c696bfe2ae52eb06e
Tommy Carstensen <tommy.carstensen@gmail.com>
2017-01-13 12:30:14 +0000

    Took into account modified cluster sampling from Hologic batches to Sanger plates.

--- projects/adrp1/samples_for_lanes.py
+++ projects/adrp1/samples_for_lanes.py
@@ -39,18 +39,19 @@ for row_idx in range(1, xl_sheet.nrows):
     pop = Population = xl_sheet.cell(row_idx, 1).value
     cb = Coriell_Batch = xl_sheet.cell(row_idx, 2).value
     hb = Hologic_Batch = xl_sheet.cell(row_idx, 3).value
+    well = Hologic_Batch = xl_sheet.cell(row_idx, 5).value
     d[hb][cb].append(ID)
     d[hb][cb].append(ID)
     d_ID2pop[ID] = pop
     plate = ((hb-1)//2)+1
+    if plate == 6 and int(well[1:]) >= 10:
+        plate = 7
     d_plate[plate]['init'][pop] += 2
     d_plate[plate]['curr'][pop] += 2
 
 for plate in range(1, n_plates+1):
-    if plate == n_plates:
-        n_lanes = int((n_samples % n_wells) / n_average_samples_per_lane)
-    else:
-        n_lanes = int(n_wells / n_average_samples_per_lane)
+    n_samples = int(sum(d_plate[plate]['init'].values()) / 2)
+    n_lanes = int(n_samples / n_average_samples_per_lane)
     for lane in range(1, n_lanes+1):
         selected_pops = []
         for hb in range(plate*2-1, min(plate*2, n_hb)+1):
